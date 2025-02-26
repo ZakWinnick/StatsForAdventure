d64a14f0636f099e261fc0168d7386456cc136d0
Zak Winnick <zak@winnick.io>
2025-02-26 14:45:17 -0800

    Remove a25SfA226

--- backend/api/__init__.py
+++
@@ -1,5 +0,0 @@
-"""API package."""
-
-from .auth import router as auth_router
-from .vehicles import router as vehicles_router
-from .commands import router as commands_router
--- backend/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Schemas package."""
-
-from .auth import RivianAuthRequest, OTPVerificationRequest, AuthResponse
-from .session import SessionData, RivianTokens, VehicleBasicInfo
-from .vehicle import VehicleState, VehicleCommandRequest, VehicleCommandResponse, ChargingSessionData
